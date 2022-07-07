1ef56eef5bc2eb206c85242e25391df59271e59e
Leonel Retamar <leonel.retamar33@gmail.com>
2022-07-07 11:41:20 -0300

    diseño de la calculadora

--- src/components/Form/Formulario.jsx
+++ src/components/Form/Formulario.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import style from "../Form/Formulario.module.css"
 
 
 const Formulario = () => {
@@ -45,24 +46,30 @@ const Formulario = () => {
 }
 
     return (
-    <div>
-        <div>
+    <div className={style.container}>
+        <div className={style.global}>
             <form onSubmit={e => handleOnSubmit(e)}>
-                <label>Inversion inicial: <input type="number" value={datos.inversion} onChange={e => handleOnChange(e)} name="inversion" pattern="^[0-9,.$]*$" required></input>$</label>
+                <div>
+                <label>Inversion inicial: <input type="number" value={datos.inversion} onChange={e => handleOnChange(e)} name="inversion" pattern="^[0-9,.$]*$" required></input></label>
                 <label>Cantidad de meses: <input type="number" value={datos.meses} onChange={e => handleOnChange(e)} name="meses" pattern="^[0-9,$]*$" min="1" required></input></label>
-                <label>Rendimiento mensual: <input type="number" value={datos.rendimiento} onChange={e => handleOnChange(e)} name="rendimiento" pattern="^[0-9,.$]*$" required></input>%</label>
-                <label>Aumento mensual de inversion: <input type="number" value={datos.aumento} onChange={e => handleOnChange(e)} name="aumento" pattern="^[0-9,$]*$" min="0" required></input>$</label>
+                <label>Rendimiento mensual (%): <input type="number" value={datos.rendimiento} onChange={e => handleOnChange(e)} name="rendimiento" pattern="^[0-9,.$]*$" required></input></label>
+                <label>Aumento mensual de inversion: <input type="number" value={datos.aumento} onChange={e => handleOnChange(e)} name="aumento" pattern="^[0-9,$]*$" min="0" required></input></label>
                 <button type="submit">calcular</button>
+                </div>
             </form>
-            <p>{datos.inversion ? "Inversion inicial: " + datos.inversion : null}</p>
-            <p>{datos.meses ? "Lapso de tiempo (en meses): " + datos.meses : null}</p>
-            <p>{datos.rendimiento ? "Porcentaje de rendimiento mensual: " + datos.rendimiento : null}</p>
-            <p>{datos.aumento ? "Deposito mensual en inversion: " + datos.aumento : null}</p>
-            <h4>{resultados.resultado ? "Total bruto al finalizar el plazo: " + resultados.resultado.toFixed(2) : null}</h4>
-            <h4>{resultados.inversionTotal ? "Inversion Total al finalizar plazo: " + resultados.inversionTotal.toFixed(2) : null}</h4>
-            <h4>{resultados.ganancias ? "Ganancias totales al finalizar plazo: " + resultados.ganancias.toFixed(2) : null}</h4>
+            <p>{datos.inversion ? `Inversion inicial: ${datos.inversion} $` : null}</p>
+            <p>{datos.meses ? `Lapso de tiempo (en meses): ${datos.meses}` : null}</p>
+            <p>{datos.rendimiento ? `Rendimiento mensual: ${datos.rendimiento} %` : null}</p>
+            <p>{datos.aumento ? `Deposito mensual en inversion: ${datos.aumento} $` : null}</p>
+            <h4>{resultados.resultado ? `Total (Bruto): ${resultados.resultado.toFixed(2)} $` : null}</h4>
+            <h4>{resultados.inversionTotal ? `Inversion Total: ${resultados.inversionTotal.toFixed(2)} $` : null}</h4>
+            <h4>{resultados.ganancias ? `Ganancias totales (Neto): ${resultados.ganancias.toFixed(2)} $` : null}</h4>
 
-            <div>{resultados.rendimientoMensual?.map((e,i) => (<p key={i+1}>{`Ganancias del mes ${i+1}: ${e.toFixed(2)}`}</p>) )}</div>
+            <div className={style.yield_container}>
+                <div>
+                    {resultados.rendimientoMensual?.map((e,i) => (<p key={i+1} className={i % 2 === 0? style.yield : style.yield2}>{`Ganancias del mes ${i+1}: `} <b>{e.toFixed(2)} $</b> </p>) )}
+                </div>
+            </div>
         </div>
     </div>
     )
